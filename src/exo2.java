a671ab73ba9249db0c9ed133ebef9c9b15662a91
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-08 16:31:28 +0200

    exo 2

---
+++ src/exo2.java
@@ -0,0 +1,10 @@
+public class exo2 {
+    public static void main(String[] args) {
+
+        System.out.println("Donner une valeur au diamètre");
+        float diametre=10F;
+        float rayon=diametre/2;
+        float surface=rayon*rayon*3.14F;
+        System.out.println(surface);
+    }
+}
