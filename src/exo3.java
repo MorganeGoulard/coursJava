984a3f11227565189fa14ce5ba1d60a6bf3c5822
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-08 16:52:06 +0200

    exo 3

---
+++ src/exo3.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class exo3 {
+    public static void main(String[] args) {
+
+        System.out.println("Saisir votre nom");
+        String nom="Bidule";
+        System.out.println("Saisir votre prenom");
+        String prenom="Pétunia";
+        System.out.println("Saisir votre âge");
+        byte age=26;
+        System.out.println("Vous vous appelez " +prenom+" "+nom+" et vous avez "+age+" ans.");
+    }
+}
