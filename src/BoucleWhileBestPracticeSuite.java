f733d75258feb8d43bd8c5e2dbff48892eaba8d6
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-10 11:48:56 +0200

    boucle while best practice suite

---
+++ src/BoucleWhileBestPracticeSuite.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BoucleWhileBestPracticeSuite {
+    public static void main(String[] args) {
+
+        var input=new Scanner(System.in);
+        // DRY : Do not repeat Yourself
+        var codePIN=""; // pour var, toujours donner une valeur initiale
+        System.out.println("Veuillez définir votre code pin à 4 chiffres : ");
+        while(true){
+            System.out.print("Code PIN : ");
+            codePIN=input.nextLine();
+            if(codePIN.length()==4){
+                System.out.println("Code PIN défini avec succès!");
+                break;
+            }
+            System.out.println("Erreur! Code PIN doit être composé de 4 caractères");
+
+
+            }
+
+
+
+        System.out.println("*******************");
+        System.out.println("Téléphone vérouillé");
+        System.out.println("*******************");
+
+        String codeATester="";
+
+        while(true){ // ici on utilise equals car c'est un String
+            System.out.print("Entrez votre code PIN pour déverouiller l'appareil: ");
+            codeATester=input.nextLine();
+            if(codeATester.equals(codePIN)){
+                System.out.println("Bienvenue!");
+                break;
+            }else{
+                System.out.println("code PIN erroné, veuillez recommencer");
+            }
+        }
+
+    }
+}
