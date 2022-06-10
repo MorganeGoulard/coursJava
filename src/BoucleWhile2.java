d5d27364adfcf2b613965713cc953a8e1f42d6d9
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-10 11:02:21 +0200

    boucle while v2

---
+++ src/BoucleWhile2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BoucleWhile2 {
+    public static void main(String[] args) {
+        var input=new Scanner(System.in);
+        var codePIN="";
+        System.out.println("Veuillez définir votre code pin à 4 chiffres : ");
+        while(codePIN.length()!=4){
+            System.out.print("Code PIN : ");
+            codePIN=input.nextLine();
+            if(codePIN.length()!=4){
+                System.out.println("Erreur! Code PIN doit être composé de 4 caractères");
+            }else{
+                System.out.println("Code PIN défini avec succès!");
+            }
+        }
+
+
+        System.out.println("*******************");
+        System.out.println("Téléphone vérouillé");
+        System.out.println("*******************");
+
+        String codeATester="";
+
+        while(!codeATester.equals(codePIN)){
+            System.out.print("Entrez votre code PIN pour déverouiller l'appareil: ");
+            codeATester=input.nextLine();
+            if(codeATester.equals(codePIN)){
+                System.out.println("Bienvenue!");
+            }else{
+                System.out.println("code PIN erroné, veuillez recommencer");
+            }
+        }
+
+    }
+}
