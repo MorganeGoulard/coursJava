bef99b2ed3de4130a340d9857c1d049e4d12390c
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-14 11:45:11 +0200

    hashTable et exo21(ou22?) avec méthode HashMap

---
+++ src/Cours/HashTable.java
@@ -0,0 +1,36 @@
+package Cours;
+
+import java.util.HashMap;
+
+public class HashTable {
+    public static void main(String[] args) {
+
+        /*HashMap<String, String> identite=new HashMap<>();
+        identite.put("nom", "Lisangola");
+        identite.put("prenom","Christian");
+        System.out.println(identite.get("nom"));*/
+
+        String phrase="Je suis dans une joie immense";
+
+        HashMap<Character, Integer> occurenceLettre=new HashMap<>();
+
+        System.out.println(occurenceLettre);
+
+
+        for (int i = 0; i < phrase.length(); i++) {
+          //  System.out.println(phrase.charAt(i));
+            char key=phrase.charAt(i);
+            if (occurenceLettre.containsKey(key)){
+                int previousValue=occurenceLettre.get(key);
+                occurenceLettre.replace(key,previousValue+1);
+            }else{
+                occurenceLettre.put(key,1);
+            }
+        }
+
+        System.out.println(occurenceLettre);
+        System.out.println("occurence de s : "+occurenceLettre.get('s'));
+
+
+    }
+}
