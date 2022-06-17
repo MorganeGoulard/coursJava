4abee0e9e37051680bebfd0a4d0d1868b6a3a248
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-17 13:53:45 +0200

    exo4

---
+++ src/tp_java_poo/exo_algo/Exo4.java
@@ -0,0 +1,44 @@
+package tp_java_poo.exo_algo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exo4 {
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Entrez votre phrase : ");
+        var str = input.nextLine();
+        char [] charStr = str.toCharArray();
+        //ArrayList<Character> strArr= new ArrayList<>();
+
+        // si un caractère à la position est un espace, mettre le caractère à la position i+1 en maj
+
+        // version Mickaël
+        boolean espace=true;
+        for (int i = 0; i < str.length()-1; i++) {
+            if (espace){
+                charStr[i] = Character.toUpperCase(charStr[i]);
+                espace=false;
+            }
+            if(charStr[i] == ' '){
+                espace=true;
+            }
+        }
+
+        System.out.println(charStr);
+
+        // version initiale
+        for (int i = 0; i < str.length()-1; i++) {
+            if(charStr[i] == ' '){
+                charStr[i+1]= Character.toUpperCase(charStr[i+1]);
+            } else if (i==0) {
+                charStr[i]= Character.toUpperCase(charStr[i]);
+            }
+        }
+
+        System.out.println(charStr);
+
+    }
+}
