c57a47d499f7afa0fa4c0938e5d25acbd6efb5f9
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-17 16:29:55 +0200

    exo 7 avec mÃméthode

---
+++ src/tp_java_poo/exo_algo/Exo8.java
@@ -0,0 +1,8 @@
+package tp_java_poo.exo_algo;
+
+public class Exo8 {
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/tp_java_poo/exo_algo/exo7.java
+++ src/tp_java_poo/exo_algo/exo7.java
@@ -3,8 +3,27 @@ package tp_java_poo.exo_algo;
 import java.util.Scanner;
 
 public class exo7 {
+
+    public static char [] majAlternee(char [] tabCaractere){
+        var tabCaractere2 = new char[tabCaractere.length];
+        for (int i = 0; i < tabCaractere.length; i++) {
+            if(i %2 == 0){
+                tabCaractere2[i] = Character.toUpperCase(tabCaractere[i]);
+            } else {
+                tabCaractere2[i]= Character.toLowerCase(tabCaractere[i]);
+            }
+        }
+        return tabCaractere2;
+    }
+
     public static void main(String[] args) {
 
+        char [] tabEx  = {'a', 'b', 'c', 'd', 'e'};
+        System.out.println(Arrays.toString(tabEx));
+        System.out.println(majAlternee(tabEx));
+
+
+        /* méthode triche
 
         char[] tabCara = {'a', 'b', 'c', 'd', 'e'};
         char[] tabCara2 = tabCara;
@@ -13,7 +32,7 @@ public class exo7 {
             tabCara2[i] = Character.toUpperCase(tabCara2[i]);
         }
 
-        System.out.println(tabCara2);
+        System.out.println(tabCara2);*/
 
     }
 }
