8c17aae522be7824ab27725be183adf2b156b845
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-18 19:47:07 +0200

    exo 8

--- src/tp_java_poo/exo_algo/Exo8.java
+++ src/tp_java_poo/exo_algo/Exo8.java
@@ -1,8 +1,46 @@
 package tp_java_poo.exo_algo;
 
+import java.util.Arrays;
+
 public class Exo8 {
+    public static int [] voirPosition (String [] tabDirection) {
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < tabDirection.length; i++) {
+            if (tabDirection[i].equals("haut")) {
+                y=y+1;
+            }
+            if (tabDirection[i].equals("bas")) {
+                y=y-1;
+            }
+            if (tabDirection[i].equals("gauche")) {
+                x=x-1;
+            }
+            if (tabDirection[i].equals("droite")) {
+                x=x+1;
+            }
+        }
+        int[] position = {x, y};
+        return position;
+
+
+    }
+
     public static void main(String[] args) {
 
+        String [] maPosition = {
+                "haut",
+                "haut",
+                "bas",
+                "gauche",
+                "gauche",
+                "droite",
+                "haut"
+        };
+
+        System.out.println(Arrays.toString(maPosition));
+        System.out.println("La position est : "+ Arrays.toString(voirPosition(maPosition)));
 
     }
 }
