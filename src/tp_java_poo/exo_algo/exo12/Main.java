060af5632e769759707db1b3f3f27e511c4a58a6
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-20 00:09:58 +0200

    exo 12

---
+++ src/tp_java_poo/exo_algo/exo12/Main.java
@@ -0,0 +1,28 @@
+package tp_java_poo.exo_algo.exo12;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Souris souris1 = new Souris(50, "blanche", 2);
+        Souris souris2 = new Souris(45, "grise");
+        Souris souris2Clonee = new Souris(souris2);
+
+        System.out.println(souris1);
+        System.out.println(souris2);
+        System.out.println(souris2Clonee);
+
+        souris1.veillir(36);
+        souris2.veillir(36);
+        souris2Clonee.veillir(28);
+        System.out.println(souris1);
+        System.out.println(souris2);
+        System.out.println(souris2Clonee);
+
+
+
+
+
+    }
+
+}
