cffb93f625c657d96c9fd9e9d423ba4898696c1a
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-19 13:01:21 +0200

    exo 10 terminé

---
+++ src/tp_java_poo/exo_algo/exo10/TestTirelire.java
@@ -0,0 +1,29 @@
+package tp_java_poo.exo_algo.exo10;
+
+public class TestTirelire {
+    public static void main(String[] args) {
+
+        Tirelire maTirelire = new Tirelire(350);
+        maTirelire.afficher();
+
+        maTirelire.remplir(100);
+        maTirelire.afficher();
+
+        maTirelire.puiser(25);
+        maTirelire.afficher();
+
+        maTirelire.secouer();
+
+        maTirelire.calculerSolde(500);
+        maTirelire.calculerSolde(125);
+        maTirelire.afficher();
+
+        maTirelire.vider();
+        System.out.println(maTirelire.afficher());
+
+
+
+
+
+    }
+}
--- src/tp_java_poo/exo_algo/exo10/Tirelire.java
+++ src/tp_java_poo/exo_algo/exo10/Tirelire.java
@@ -1,11 +1,13 @@
 package tp_java_poo.exo_algo.exo10;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Tirelire {
 
     private double solde;
 
-    public Tirelire(double maTirelire) {
-        this.solde = maTirelire;
+    public Tirelire(double tirelire) {
+        this.solde = tirelire;
     }
 
     public double getMontant(){
@@ -14,16 +16,18 @@ public class Tirelire {
 
     public String afficher(){
          if (this.solde > 0) {
-            System.out.printf("vous avez %f € dans votre tirelire.",this.solde);
+            System.out.printf("vous avez %.2f € dans votre tirelire.\n",this.solde);
         } else if (this.solde < 0) {
             System.out.println("vous êtes à découvert :'(");
         }
-        return "vous êtes sans le sou.";
+        return "vous êtes sans le sou";
     }
 
     public void secouer(){
         if (this.solde > 0){
             System.out.println("bling bling vous êtes riche\n");
+        } else {
+            System.out.println("*pas de bruit*");
         }
     }
 
@@ -33,6 +37,31 @@ public class Tirelire {
         }
         return this.solde;
     }
+
+    public double vider(){
+        return this.solde = 0;
+    }
+
+    public void puiser(double montantAPuiser){
+        if(montantAPuiser > this.solde){
+            System.out.println("vous n'avez pas assez d'argent pour cette opération");
+        } else {
+            this.solde=this.solde - montantAPuiser;
+        }
+    }
+
+    public double calculerSolde(double budget){
+        if(budget<this.solde){
+            this.solde=this.solde - budget;
+            System.out.printf("vous êtes assez riche pour partir en vacances, il vous restera %.2f à votre retour\n", this.solde);
+        } else if(budget>this.solde) {
+            System.out.printf("vous n'avez pas assez d'argent pour partir en vacances, il vous manque %.2f\n",(budget-this.solde));
+        }
+        return this.solde;
+    }
+
+
+
 
 
 
