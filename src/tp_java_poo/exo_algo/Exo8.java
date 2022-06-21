86ed2e6359888642da3b19a5f72676d20562c87b
Morgane Goulard <morganegoulard.56@gmail.com>
2022-06-21 08:44:15 +0200

    commit

--- src/tp_java_poo/exo_algo/Exo8.java
+++ src/tp_java_poo/exo_algo/Exo8.java
@@ -1,6 +1,12 @@
 package tp_java_poo.exo_algo;
 
 public class Exo8 {
+
+    public static int [] avoirPosition (String [] direction){
+        int [] position = new int[]{0,0};
+
+    }
+
     public static void main(String[] args) {
 
 
